91f26871141c398dfa2edfed3375d82494b3571e
Jaables <jamie.cs.bell@hotmail.com>
2022-08-04 21:25:44 +0100

    Added functionality to view used words.

--- main.py
+++ main.py
@@ -1,13 +1,26 @@
 import wordle
+import time
 
+# Use this file to cheat and find out what the word you are trying to guess is
 if __name__ == '__main__':
     with open("cheat.txt", "w") as f:
         f.write(wordle.RANDOM_WORD)
 
+    print("Welcome to my Wordle Game! You have a maximum of 5 attemps to guess "
+          "a 5 letter word, good luck! (Type 'help' for a list of used letters)" )
+    time.sleep(2)
+
     while True:
         guess = wordle.Guess_Word(
-            w_str=input(f'[{wordle.Guess_Word.count}]')
+            w_str=input(f'You have {wordle.Guess_Word.count} guesses remaining. Your word: ')
         )
+
+        if guess.w_str == 'help':
+            list = list(wordle.Guess_Word.alphabet.values())
+            for i in list:
+                 print(i, end=" " if list[-1] != i else "\n")
+            continue
+
         if guess.is_valid():
             guess.increment_turn()
             guess.apply_guesses()
--- wordle.py
+++ wordle.py
@@ -3,7 +3,7 @@ import random
 import sys
 
 RANDOM_WORD = random.choice(valid_words)
-GUESSES_MAX = 5
+GUESSES_MAX = 0
 
 
 class Colour:
@@ -14,17 +14,49 @@ class Colour:
 
 
 class Guess_Word:
-    count = 0
+    count = 5
+    store_words = []
+    alphabet = {
+        "a": "a",
+        "b": "b",
+        "c": "c",
+        "d": "d",
+        "e": "e",
+        "f": "f",
+        "g": "g",
+        "h": "h",
+        "i": "i",
+        "j": "j",
+        "k": "k",
+        "l": "l",
+        "m": "m",
+        "n": "n",
+        "o": "o",
+        "p": "p",
+        "q": "q",
+        "r": "r",
+        "s": "s",
+        "t": "t",
+        "u": "u",
+        "v": "v",
+        "w": "w",
+        "x": "x",
+        "y": "y",
+        "z": "z",
+    }
     def __init__(self, w_str):
         self.w_str = w_str
         self.w_chars = list(self.w_str)
         self.guess_str = ""
 
     def increment_turn(self):
-        Guess_Word.count += 1
+        Guess_Word.count -= 1
 
     def is_valid(self):
-        return self.w_str in valid_words
+        if self.w_str in valid_words:
+            return self.w_str
+        else:
+            print("Sorry that word doesn't exist. Try again: ")
 
     def green(self):
         for i, _ in enumerate(self.w_chars):
@@ -33,24 +65,42 @@ class Guess_Word:
             if actual_char == guessed_char:
                 coloured_char = f"{Colour.GREEN}{actual_char}{Colour.BASE}"
                 self.w_chars[i] = coloured_char
+                self.edit_alphabet(actual_char, coloured_char)
+
 
     def yellow(self):
         for i, _ in enumerate(self.w_chars):
             guessed_char = self.w_chars[i]
-            if guessed_char in RANDOM_WORD:
+            not_green = Colour.GREEN not in Guess_Word.alphabet.get(guessed_char, '')
+            if guessed_char in RANDOM_WORD and not_green:
                 coloured_char = f"{Colour.YELLOW}{guessed_char}{Colour.BASE}"
                 self.w_chars[i] = coloured_char
+                self.edit_alphabet(guessed_char, coloured_char)
+            else:
+                coloured_char = f"{Colour.RED}{guessed_char}{Colour.BASE}"
+                self.edit_alphabet(guessed_char, coloured_char)
 
+    def edit_alphabet(self, k, v):
+        # Stops additional keys being added
+        if k not in Guess_Word.alphabet.keys():
+            return
+        Guess_Word.alphabet[k] = v
 
     def apply_guesses(self):
         self.green()
         self.yellow()
         self.guess_str = ''.join(self.w_chars)
+        Guess_Word.store_words.append(self.guess_str)
         print(self.guess_str)
+
+
+
 
     def guessed_word(self):
         if self.w_str == RANDOM_WORD:
-            print(f"Congrats you have beat Wordle in {Guess_Word.count} tries!")
+            print(f"Congrats you have beat Wordle with {Guess_Word.count} attempts remaining! Your guessed words were:")
+            for i in Guess_Word.store_words:
+                print(i)
             sys.exit(1)
 
     def game_loss(self):
